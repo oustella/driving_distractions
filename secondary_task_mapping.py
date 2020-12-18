97f498701427b7dc16ac81dad962af1d02f2ca2a
oustella <oustella@users.noreply.github.com>
2020-12-18 12:36:33 -0500

    Create secondary_task_mapping.py

---
+++ secondary_task_mapping.py
@@ -0,0 +1,86 @@
+# secondary tasks mapping
+
+ST_dict = {
+    "Interaction": [
+        "Child in rear seat - interaction",
+        "Passenger in adjacent seat - interaction",
+        "Passenger in rear seat - interaction",
+        "Child in adjacent seat - interaction",
+        "Pet in vehicle"
+    ],
+    "Entertainment": [
+        "Talking/singing, audience unknown",
+        "Dancing",
+        "Writing",
+        "Reading"
+    ],
+    "Mobiledevice": [
+        "Cell phone, Dialing hand-held",
+        "Cell phone, Dialing hands-free using voice-activated software",
+        "Cell phone, holding",
+        "Cell phone, Holding",
+        "Tablet device, Locating/reaching",
+        "Tablet device, Operating",
+        "Tablet device, Viewing",
+        "Cell phone, Texting",
+        "Cell phone, Browsing",
+        "Cell phone, Talking/listening, hand-held",
+        "Tablet device, Other",
+        "Cell phone, Locating/reaching/answering",
+        "Cell phone, other",
+        "Cell phone, Dialing hand-held using quick keys"
+    ],
+    "External": [
+        "Other external distraction",
+        "Looking at an object external to the vehicle",
+        "Distracted by construction",
+        "Looking at previous crash or incident",
+        "Looking at pedestrian",
+        "Looking at animal"  # moved from Interaction 11/17/2020
+    ],
+    "Internal": [
+        "Adjusting/monitoring other devices integral to vehicle",
+        "Moving object in vehicle",
+        "Adjusting/monitoring radio",
+        "Object in vehicle, other",
+        "Adjusting/monitoring climate control",
+        "Object dropped by driver",
+        "Inserting/retrieving CD (or similar)",
+        "Reaching for object, other",
+        "Other non-specific internal eye glance",  # moved from other 11/17/2020
+        "Insect in vehicle"  # moved from other 11/17/2020
+    ],
+    "Foodbeverage": [
+        "Drinking with straw, no lid",
+        "Drinking with lid and straw",
+        "Drinking with lid, no straw",
+        "Drinking from open container",
+        "Eating with utensils",
+        "Reaching for food-related or drink-related item",
+        "Eating without utensils"
+    ],
+    "Grooming": [
+        "Removing/inserting/ adjusting contact lenses or glasses",
+        "Other personal hygiene",
+        "Biting nails/cuticles",
+        "Combing/brushing/fixing hair",
+        "Applying make-up",
+        "Removing/adjusting jewelry",
+        "Removing/adjusting clothing",
+        "Brushing/flossing teeth",
+        "Reaching for personal body-related item",
+        "Shaving"
+    ],
+    "Smoking": [
+        "Smoking cigar/cigarette",
+        "Reaching for cigar/cigarette",
+        "Extinguishing cigar/cigarette",
+        "Lighting cigar/cigarette",
+    ],
+    "Notask": ["No Secondary Tasks", "No Additional Secondary Tasks"],
+    "Other": [
+        "Other known secondary task",
+        "Unknown type (secondary task present)",
+        "Unknown",
+    ],
+}
